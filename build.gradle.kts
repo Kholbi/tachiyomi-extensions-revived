44c086781e89fbc2527e35211147db3074b3b7b2
Kholbi <101682588+Kholbi@users.noreply.github.com>
2025-04-16 16:44:17 +0800

    auk

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ buildscript {
         mavenCentral()
         google()
         maven(url = "https://plugins.gradle.org/m2/")
+        maven("https://jitpack.io")
     }
     dependencies {
         classpath(libs.gradle.agp)
